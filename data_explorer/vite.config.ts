6ec999a27d5a4377eb0ecfeb02047c3dc7cc6107
Matthew Haynes <70829360+matthewhaynesonline@users.noreply.github.com>
2026-04-19 12:06:58 -0400

    Push to HF Space

--- data_explorer/vite.config.ts
+++ data_explorer/vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/Axiom/",
+  base: process.env.DEPLOY_TARGET === "huggingface" ? "/" : "/Axiom/",
 });
